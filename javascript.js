062b5196106dcc99c2ecd78add5d8301bd52474b
IvanPagani <ivanpagani07@gmail.com>
2025-01-22 15:53:24 -0300

    Created functions to get the Computer Choice and Human Choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,38 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+function getComputerChoice() {
+
+    let random = Math.random();
+    let a;
+
+    if (random <= (1/3)) {
+        a = "rock";
+    }
+    else if (random <= (2/3)) {
+        a = "paper";
+    }
+    else {
+        a = "scissors";
+    }
+
+    return a;
+}
+
+function getHumanChoice() {
+
+    let b = prompt("Choose rock, paper or scissors.", "rock, paper or scissors");
+
+    while ( !(b === "rock" || b === "paper" || b === "scissors") ) {
+
+            console.log("That's not a valid choice.");
+            b = prompt("Choose rock, paper or scissors.", "");
+    }
+
+    return b;
+}
+
+
+let computerchoice = getComputerChoice();
+console.log("The Computer choice is: " + computerchoice);
+let humanchoice = getHumanChoice();
+console.log("The Human choice is: " + humanchoice);
