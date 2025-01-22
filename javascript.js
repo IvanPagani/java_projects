238c15aec4c035dd77a860a0eb9e9118b6e02009
IvanPagani <ivanpagani07@gmail.com>
2025-01-22 16:43:55 -0300

    Added the function playRound and made minor modifications to the code.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+console.log("Rock Paper Scissors Game");
 
 function getComputerChoice() {
 
@@ -21,18 +21,69 @@ function getComputerChoice() {
 function getHumanChoice() {
 
     let b = prompt("Choose rock, paper or scissors.", "rock, paper or scissors");
+    b = b.toLowerCase();
 
     while ( !(b === "rock" || b === "paper" || b === "scissors") ) {
 
             console.log("That's not a valid choice.");
             b = prompt("Choose rock, paper or scissors.", "");
+            b = b.toLowerCase();
     }
 
     return b;
 }
 
+// "a" is the computer choice
+// "b" is the human choice
+
+function playRound (a, b) {
+    console.log("The Computer choice is: " + a + ".");
+    console.log("The Human choice is: " + b + ".");
+
+    if (a === "rock") {
+        if (b === "scissors") {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore += 1;
+        }
+        else if (b === "paper") {
+            console.log("You win! Paper beats Rock.");
+            humanScore += 1;
+        }
+        else {
+            console.log("Tie!");
+        }
+    }
+    else if (a === "paper") {
+        if (b === "rock") {
+            console.log("You lose! Paper beats Rock.");
+            computerScore += 1;
+        }
+        else if (b === "scissors") {
+            console.log("You win! Scissors beats Paper.");
+            humanScore += 1;
+        }
+        else {
+            console.log("Tie!");
+        }
+    }
+    else {
+        if (b === "paper") {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore += 1;
+        }
+        else if (b === "rock") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore += 1;
+        }
+        else {
+            console.log("Tie!");
+        }
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
 
 let computerchoice = getComputerChoice();
-console.log("The Computer choice is: " + computerchoice);
 let humanchoice = getHumanChoice();
-console.log("The Human choice is: " + humanchoice);
+let play = playRound(computerchoice, humanchoice);
