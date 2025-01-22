7ebdadc6d73d3d419266e36a3c49af152d0a9c37
IvanPagani <ivanpagani07@gmail.com>
2025-01-22 17:19:18 -0300

    Created the playGame function and placed the playRound function inside of it. Code is finished.

--- javascript.js
+++ javascript.js
@@ -36,54 +36,65 @@ function getHumanChoice() {
 // "a" is the computer choice
 // "b" is the human choice
 
-function playRound (a, b) {
-    console.log("The Computer choice is: " + a + ".");
-    console.log("The Human choice is: " + b + ".");
+function playGame() {
 
-    if (a === "rock") {
-        if (b === "scissors") {
-            console.log("You lose! Rock beats Scissors.");
-            computerScore += 1;
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound (a, b) {
+        console.log("The Computer choice is: " + a + ".");
+        console.log("The Human choice is: " + b + ".");
+    
+        if (a === "rock") {
+            if (b === "scissors") {
+                console.log("You lose! Rock beats Scissors.");
+                computerScore += 1;
+            }
+            else if (b === "paper") {
+                console.log("You win! Paper beats Rock.");
+                humanScore += 1;
+            }
+            else {
+                console.log("Tie!");
+            }
         }
-        else if (b === "paper") {
-            console.log("You win! Paper beats Rock.");
-            humanScore += 1;
+        else if (a === "paper") {
+            if (b === "rock") {
+                console.log("You lose! Paper beats Rock.");
+                computerScore += 1;
+            }
+            else if (b === "scissors") {
+                console.log("You win! Scissors beats Paper.");
+                humanScore += 1;
+            }
+            else {
+                console.log("Tie!");
+            }
         }
         else {
-            console.log("Tie!");
+            if (b === "paper") {
+                console.log("You lose! Scissors beats Paper.");
+                computerScore += 1;
+            }
+            else if (b === "rock") {
+                console.log("You win! Rock beats Scissors.");
+                humanScore += 1;
+            }
+            else {
+                console.log("Tie!");
+            }
         }
     }
-    else if (a === "paper") {
-        if (b === "rock") {
-            console.log("You lose! Paper beats Rock.");
-            computerScore += 1;
-        }
-        else if (b === "scissors") {
-            console.log("You win! Scissors beats Paper.");
-            humanScore += 1;
-        }
-        else {
-            console.log("Tie!");
-        }
-    }
-    else {
-        if (b === "paper") {
-            console.log("You lose! Scissors beats Paper.");
-            computerScore += 1;
-        }
-        else if (b === "rock") {
-            console.log("You win! Rock beats Scissors.");
-            humanScore += 1;
-        }
-        else {
-            console.log("Tie!");
-        }
+
+    for (let i = 0; i <5; i++) {
+        
+        let computerchoice = getComputerChoice();
+        let humanchoice = getHumanChoice();
+
+        let play = playRound(computerchoice, humanchoice);
+
+        console.log("Computer " + computerScore + " vs " + humanScore + " Human");
     }
 }
 
-let computerScore = 0;
-let humanScore = 0;
-
-let computerchoice = getComputerChoice();
-let humanchoice = getHumanChoice();
-let play = playRound(computerchoice, humanchoice);
+let game = playGame();
